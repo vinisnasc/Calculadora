e84fb442e036d93d91bd90181b3bf36af8cae524
viniciusnasc <vini.souza00@gmail.com>
2021-05-16 17:19:51 -0300

    Comentarios

---
+++ Calculadora/calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Algoritmo de cálculo de alcance de publicidade.
+    /// </summary>
+    class Calculadora
+    {
+        static void Main(string[] args)
+        {
+            // Define o valor do investimento.
+            Console.WriteLine("Qual o valor investido?");
+            double invest = double.Parse(Console.ReadLine());
+
+            // Calcula a quantidade de visualização de acordo com o valor investido,
+            // sendo considerado quatro o maximo de compartilhamento no loop.
+            double vis = invest * 30;
+            double nvis = vis;
+            double totalvis = vis;
+
+            for (int i = 0; i < 4; i++)
+            {
+                double clique = nvis * 0.12;
+                double comp = clique * 0.15;
+                nvis = comp * 40;
+                totalvis += nvis;
+            }
+
+            // Printa o resultado do cálculo, apresentando o valor investido e o alcance da publicação.
+            Console.WriteLine($"\nCom o valor de R$ {invest} investido, " +
+                $"o total de visualizações será de aproximadamente {Math.Round(totalvis)}.");
+        }
+    }
+}
+
+        
